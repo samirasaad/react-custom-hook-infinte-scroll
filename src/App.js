e7274481ff843a0c3b91e86b092afe449404d407
Samira <samirasaad@gmail.com>
2024-03-21 15:06:13 +0200

    creating custom hook to invoke a call back function on scrolling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { axiosInstance } from "./axios/axiosInstance";
+import { useState } from "react";
+import useInfiniteScroll from "./hooks/UseInfinteScroll";
+import "./App.css";
 
+// skip => should equals number of limit as dummy json desont contain page keyword for pages
 function App() {
+  const [page, setPage] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [paginationObj, setPaginationObj] = useState({});
+
+  const fetchData = async () => {
+    try {
+      setILoading(true);
+      const res = await axiosInstance.get(
+        `/products?limit=30&skip=${page}&delay=1500`
+      );
+      console.log(res);
+      if (res) {
+        setILoading(false);
+        setPage(page + 30);
+        setProducts((prevProducts) => [...prevProducts, ...res.data.products]);
+        setPaginationObj({
+          skip: res.data.skip,
+          total: res.data.total,
+          limit: res.data.limit,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const [isLoading, setILoading] = useInfiniteScroll(fetchData);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {products?.map((product) => (
+        <p
+          key={product.id}
+          style={{
+            background: "gray",
+            border: "1px solid black",
+            padding: "10px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {`${product.id}-${product.title}`}
+        </p>
+      ))}
+      {isLoading && <p>loading.........</p>}
+      {paginationObj.total === products.length && (
+        <p style={{ fontWeight: "bold" }}>you reached the end of the results</p>
+      )}
     </div>
   );
 }
---
+++ src/hooks/UseInfinteScroll.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useInfiniteScroll = (functionWillBeInvokedOnScroll) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) return;
+    functionWillBeInvokedOnScroll();
+  }, [isLoading]);
+
+  function handleScroll() {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      isLoading
+    )
+      return;
+    setIsLoading(true);
+  }
+  return [isLoading, setIsLoading];
+};
+
+export default useInfiniteScroll;
